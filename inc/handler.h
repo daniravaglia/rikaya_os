c076d07021e20791e9b079a221ddedeb910cd4e5
Vairo Di Pasquale <vairo.dipasquale@labs.cs.unibo.it>
2019-04-17 17:05:10 +0200

    bozza handler

--- inc/handler.h
+++ inc/handler.h
@@ -4,6 +4,8 @@
 
 void sys_break_handler();
 
+void kill_process(pcb_t* proc);
+
 void prgrm_trap_handler();
 
 void tlb_mngmt_handler();
--- src/handler.c
+++ src/handler.c
@@ -3,10 +3,52 @@
 #include <umps/libumps.h>
 #include <handler.h>
 
+// Per questa fase va controlalta solo una syscall, ovvero quella che termina
+// il processo. Ecco perche viene chiamata alla fine del test. "Per gestirla 
+// dovete popolare la new area relativa a syscalle  brakpoint con handler
+// e implementare questa syscall che termina il process( rumuoverlo dalla lis
+// ta dei processi che devono essere eseguiti)"
+
+
+
 void sys_break_handler()
 {
+   //ci sono da fare ovviamente cose prima ma ad una certa c'è da fare lo switch
+   //delle syscall
+   
+   state_t* oldState;
+
+   pcb_t* process;
+  
+   //inseirire l'indirizzo della oldarea
+
+   //questo pezzo l'ho preso da coso poi lo si vede bene
+   // per adesso serve come bozza
+   int sysCallNumber = oldState->s_a0;
+
+   switch (sysCallNumber)
+   {
+      case 3:
+         kill_process();
+         break;
+
+   }
+}
+
+
+//SYSCALL 3
+
+void kill_process(pcb_t* proc)
+{
+   //fare il loop che cerca nell'albero il processo da uccidere con removeChild
+   // (il loop ricorsivo ovviamente)
+   
+
 
 }
+
+
+
 
 void prgrm_trap_handler() {}
 
